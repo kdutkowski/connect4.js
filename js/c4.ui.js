4f0ae3d3100203ad1c1b016bb5a3bbce2e009c6b
Jakub Dutkowski <kdutkowski@gmail.com>
2015-05-15 13:25:57 +0200

    removed a function that was causing crash

--- js/c4.ui.js
+++ js/c4.ui.js
@@ -10,24 +10,24 @@ C4.UI = function(_game, _options) {
 
 
 	function showLastMove() {
-
-		if (!_game.moves.length) {
-			return;
-		}
-		var move = _game.moves[_game.moves.length - 1];
-		var player = move.player;
-		var col = move.col_index;
-		var row = move.row_index;
-		console.error("Player: " + player.toString() + " Col: " + col + " Row: " + row)
-
-		var a = [];
-		for (var r = 0; r < _game.rack.length; r++) {
-			a.push('\n');
-			for (var c = 0; c < _game.rack[0].length; c++) {
-				a.push(getCellChar(c, r));
-			}
-		}
-		console.error(a.join('') + '\n');
+        // TODO: think of something better !!!
+		//if (!_game.moves.length) {
+		//	return;
+		//}
+		//var move = _game.moves[_game.moves.length - 1];
+		//var player = move.player;
+		//var col = move.col_index;
+		//var row = move.row_index;
+		//console.error("Player: " + player.toString() + " Col: " + col + " Row: " + row)
+        //
+		//var a = [];
+		//for (var r = 0; r < _game.rack.length; r++) {
+		//	a.push('\n');
+		//	for (var c = 0; c < _game.rack[0].length; c++) {
+		//		a.push(getCellChar(c, r));
+		//	}
+		//}
+		//console.error(a.join('') + '\n');
 	}
 
 	function getCellChar(c, r) {
